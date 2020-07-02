b17b8afbfde38820b41b5b5a0fc619f67b83ba14
Hunter Parks <hunter.thomas.parks@gmail.com>
2020-07-02 07:12:51 -0700

    Add Cube Generator custom react hook

--- src/App.js
+++ src/App.js
@@ -9,14 +9,49 @@ class Game {
   }
 }
 
+const useCubeGenerator = (newCount, newColor = 0x00ff00, geometry = [1, 1, 1]) => {
+  const [cubes, setCubes] = useState(new Three.Group());
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    setCount(newCount); 
+  }, [])
+
+  useEffect(() => {
+    let difference = count - cubes.children.length;
+    let offset = 0;
+    
+    if(difference) {
+      for(let i = 0; i <= difference - 1; i++) {
+        let mesh = new Three.Mesh(
+          new Three.BoxGeometry(...geometry),
+          new Three.MeshBasicMaterial( { color: newColor } )
+        );
+
+        mesh.position.x += offset;
+        offset += 2;
+
+        setCubes(() => {
+          let newCubes = cubes;
+          newCubes.add(mesh);
+          return newCubes;
+        });
+      }
+    }
+  }, [count]);
+
+  return [cubes, setCubes];
+}
+
 const App = () => {
+  const [cubes, setCubes] = useCubeGenerator(5);
+
   let game,
       scene,
       renderer,
       camera,
       geometry,
-      material,
-      cube;
+      material
 
   const Initialize = () => {
     return new Game(
@@ -32,19 +67,16 @@ const App = () => {
 
   const animate = () => {
     requestAnimationFrame( animate );
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    cubes.rotation.x += 0.01;
+    cubes.rotation.y += 0.01;
     game.renderer.render( game.scene, game.camera );
   }
 
   useEffect(() => {
-    game     = Initialize();
-    geometry = new Three.BoxGeometry(1, 1, 1);
-    material = new Three.MeshBasicMaterial( { color: 0x00ff00 } );
-    cube     = new Three.Mesh( geometry, material );
+    game = Initialize();
 
     game.renderer.setSize( window.innerWidth, window.innerHeight );
-    game.scene.add( cube );
+    game.scene.add(cubes);
     game.camera.position.z = 5;
   
     document.getElementById('game').appendChild(game.renderer.domElement);
